41c99f97e073e4642174e95bacae21767aad983b
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2024-10-22 00:22:35 +0530

    copy to clip board functionality is done

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function Widget() {
 
@@ -6,6 +6,7 @@ function Widget() {
     const [numberAllowed, setNumberAllowed] = useState(false);
     const [charactersAllowed, setCharactersAllowed] = useState(false);
     const [password, setPassword] = useState("");
+    const passwordRef = useRef(null);
 
     const generatePassword = useCallback(() => {
         let password = "";
@@ -26,13 +27,18 @@ function Widget() {
         generatePassword();
     }, [range, numberAllowed, charactersAllowed]);
 
+    const copyPasswordToClipboard = useCallback(() => {
+        window.navigator.clipboard.writeText(password);
+        (passwordRef.current as unknown as HTMLInputElement).select();
+    }, [password])
+
     return (
         <div className="mt-10 p-4 rounded-lg text-orange-900 h-fit w-1/3 bg-blue-400">
             <h1>Password generator widget</h1>
             <div className="w-100">
                 <div className="flex">
-                    <input className="border rounded-s-lg w-5/6 p-1" type="text" data-testid="password" value={password} readOnly/>
-                    <button className="bg-blue-600 rounded-e-lg px-3 h-100 w-1/6 border-blue-600 border text-white">Copy</button>
+                    <input className="border rounded-s-lg w-5/6 p-1" type="text" data-testid="password" value={password} readOnly ref={passwordRef}/>
+                    <button className="bg-blue-600 rounded-e-lg px-3 h-100 w-1/6 border-blue-600 border text-white" onClick={copyPasswordToClipboard}>Copy</button>
                 </div>
                 <div className="flex gap-3 mt-2 w-100">
                     <div className="w-2/4">
